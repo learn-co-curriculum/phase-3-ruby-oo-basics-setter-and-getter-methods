26046021295d80be5b029aaa32d45b43e3746524
Jason Decker <jasondecker@Portatile-Flatiron.local>
2017-11-20 17:25:25 -0500

    Update solution to remove attr_accessor

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-  attr_accessor :name, :breed
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
 end
