e11ce7d0e9bc0fb75687f485d497b8cf666bf237
Jason Decker <jasondecker@Portatile-Flatiron.local>
2017-11-20 17:25:25 -0500

    Update solution to remove attr_accessor

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
-  attr_accessor :name, :job
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(job)
+    @job = job
+  end
 end
