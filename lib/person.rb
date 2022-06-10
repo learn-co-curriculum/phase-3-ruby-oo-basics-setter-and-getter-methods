4adcbe9347305d77889177f09851207cef07161d
Daryl Rogony <darylrogony@gmail.com>
2022-06-10 12:15:04 +0300

    commit tree c996c27f696f8b44bc4cbe3e33eac9cd3daa299e

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
-
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  #job getter instance method
+  def job=(job)
+    @job=job
+  end
+  def job
+    @job
+  end
 end
