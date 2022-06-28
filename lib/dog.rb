d00ce39694a49bbc0d41203c326b30a1640c146c
Alex Guanipatin <sebas5950@gmail.com>
2022-06-28 11:06:11 -0400

    commit tree 7ac9ba5f1cdc4d4839831b226622decb04f9187c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
 
 end
+
+# sammy = Dog.new
+# sammy.name = "Sammy"
+# sammy.breed = 'Bulldog'
+
+# puts sammy.name
+# puts sammy.breed
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,28 @@
+require 'pry'
+
 class Person
 
-end
+    # setter method
+    def name=(name)
+      @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
+  
+    # getter method
+    def name
+      @name
+    end
+
+    def job
+        @job
+    end
+  
+  end
+
+# kanye = Person.new
+# kanye.name = "Kanye"
+
+# puts kanye.name
