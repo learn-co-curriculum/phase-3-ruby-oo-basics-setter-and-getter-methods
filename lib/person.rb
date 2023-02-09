c3bd5457a67dc6fd18745b0d253b5ab57e1117bc
WendyNjuguna <wendynjuguna714@gmail.com>
2023-02-10 02:40:27 +0300

    commit tree 626f912afb32045645bb2d09c7f3d52bf1f30faa

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
+    # name
+    def name=(name)
+        @name = name
+    end
 
+    # getter method
+    def name
+        @name
+    end
+
+    # job
+    # setter method
+    def job=(job)
+        @job = job
+    end
+    
+    # getter method
+    def job
+        @job
+    end
 end
