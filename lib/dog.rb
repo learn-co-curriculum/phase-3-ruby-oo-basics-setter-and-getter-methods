3e55e74e9486708b9119a9ad8d3c78c861593a67
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-05-24 15:20:51 -0700

    add get and setter methods for dog

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
 
 end
